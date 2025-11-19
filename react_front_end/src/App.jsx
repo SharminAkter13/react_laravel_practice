d87f3c9bcc5c4a8786b7fd5f8dcfe0d5724edd30
SharminAkter13 <sharminakter13092000@gmail.com>
2025-11-19 08:52:17 +0600

    projects

--- react_front_end/src/App.jsx
+++ react_front_end/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Category from './component/Category';
 import AddCategoey from './component/AddCategoey';
 import Edit from './component/Edit';
-import { NavLink } from 'react-router-dom';
 
 
 const App = () => {
