ba56f459d866e6e2df448bd4c30ccaec5763a970
SharminAkter13 <sharminakter13092000@gmail.com>
2025-11-26 10:15:58 +0600

    projects

---
+++ larave-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $token = Str::random(60);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'api_token'=> $token,
+        ]);
+
+        return response()->json([
+            'message' => 'Registration Successful',
+            'token'   => $token
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid Credentials'], 401);
+        }
+
+
+        //  to generate new Token
+        $token = Str::random(60);
+        $user->update(['api_token' => $token]);
+
+        return response()->json([
+            'message' => 'Login Successful',
+            'token'   => $token
+        ]);
+    }
+
+    public function user()
+    {
+        return response()->json(auth()->user());
+    }
+}
--- larave-backend/routes/api.php
+++ larave-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -12,3 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::resource('products', ProductController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('users', UserController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
